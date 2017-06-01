521f0185943c04aef3051768500f1aa870ce4fea
BradCafe <bradcaferra@gmail.com>
2017-06-01 20:32:12 +1000

    Log Handler finished

--- src/asgn2Restaurant/LogHandler.java
+++ src/asgn2Restaurant/LogHandler.java
@@ -2,11 +2,23 @@ package asgn2Restaurant;
 
 
 import java.util.ArrayList;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalTime;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 import asgn2Customers.Customer;
+import asgn2Customers.CustomerFactory;
 import asgn2Exceptions.CustomerException;
 import asgn2Exceptions.LogHandlerException;
 import asgn2Exceptions.PizzaException;
 import asgn2Pizzas.Pizza;
+import asgn2Pizzas.PizzaFactory;
 
 /**
  *
@@ -30,7 +42,21 @@ public class LogHandler {
 	 * 
 	 */
 	public static ArrayList<Customer> populateCustomerDataset(String filename) throws CustomerException, LogHandlerException{
-		// TO DO
+		
+		ArrayList<Customer> customerList = new ArrayList<>();
+		
+		try (Stream<String> fileLines = Files.lines(Paths.get(new File(filename).toURI()))) {
+			for (String line : fileLines.collect(Collectors.toList())) {
+				customerList.add(createCustomer(line));
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			throw new LogHandlerException("Cannot read file: " + filename);
+		}
+				
+				
+		return customerList;
+		
 	}		
 
 	/**
@@ -42,7 +68,21 @@ public class LogHandler {
 	 * 
 	 */
 	public static ArrayList<Pizza> populatePizzaDataset(String filename) throws PizzaException, LogHandlerException{
-		// TO DO
+		
+		ArrayList<Pizza> pizzaList = new ArrayList<>();
+		
+		try (Stream<String> fileLines = Files.lines(Paths.get(new File(filename).toURI()))) {
+			for (String line : fileLines.collect(Collectors.toList())) {
+				pizzaList.add(createPizza(line));
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			throw new LogHandlerException("Cannot read file: " + filename);
+		}
+				
+				
+		return pizzaList;
+		
 	}		
 
 	
@@ -55,7 +95,24 @@ public class LogHandler {
 	 * @throws LogHandlerException - If there was a problem parsing the line from the log file.
 	 */
 	public static Customer createCustomer(String line) throws CustomerException, LogHandlerException{
-		// TO DO
+		
+		String[] lineValues;
+		
+		try{
+			lineValues = line.split(",");
+		} catch(Exception ex) {
+			throw new LogHandlerException("Encountered problem pasring line" + ex);
+		}
+		
+		try{
+			return CustomerFactory.getCustomer(lineValues[4], lineValues[2], lineValues[3], Integer.parseInt(lineValues[5]), Integer.parseInt(lineValues[6]));
+		} catch(CustomerException ex){
+			throw new LogHandlerException("Invalid Customer code" + ex);
+		}
+		catch (Exception ex){
+			throw new LogHandlerException("Line input:" + line + " is not valid " + ex);
+		}
+		
 	}
 	
 	/**
@@ -67,7 +124,57 @@ public class LogHandler {
 	 * @throws LogHandlerException - If there was a problem parsing the line from the log file.
 	 */
 	public static Pizza createPizza(String line) throws PizzaException, LogHandlerException{
-		// TO DO		
+		
+		String[] lineValues;
+		try{
+			lineValues = line.split(",");
+		} catch(Exception ex) {
+			throw new LogHandlerException("Encountered problem pasring line" + ex);
+		}
+
+		String pizzaCode = lineValues[7];
+				
+		int quantity = 0;
+		try {
+			quantity = Integer.parseInt(lineValues[8]);
+		} catch (Exception ex) {	
+			throw new LogHandlerException("Line input:" + line + " is not valid " + ex);
+		}
+		
+		//Order Time as Local Time
+		String[] time = lineValues[0].split(":");
+		if (time.length < 3) {
+			throw new LogHandlerException("Not enough time passed");
+		}
+		LocalTime orderTime = null;
+		try {
+			int timeHour = Integer.parseInt(time[0]);
+			int timeMinute = Integer.parseInt(time[1]);
+			int timeSecond = Integer.parseInt(time[2]);
+			orderTime = LocalTime.of(timeHour, timeMinute, timeSecond);
+		} catch (Exception ex) {
+			throw new LogHandlerException("Line input:" + line + " is not valid " + ex);
+		}
+		
+		//Delivery Time as Local Time
+		time = lineValues[1].split(":");
+		if (time.length < 3) {
+			throw new LogHandlerException("Not enough time passed");
+		}
+		LocalTime deliveryTime = null;
+		try {
+			int timeHour = Integer.parseInt(time[0]);
+			int timeMinute = Integer.parseInt(time[1]);
+			int timeSecond = Integer.parseInt(time[2]);
+			deliveryTime = LocalTime.of(timeHour, timeMinute, timeSecond);
+		} catch (Exception ex) {
+			throw new LogHandlerException("Line input:" + line + " is not valid " + ex);
+		}
+
+		return PizzaFactory.getPizza(pizzaCode, quantity, orderTime, deliveryTime);		
+		
+		
+		
 	}
 
 }
